26e39419d17e654cfaf82f188fe0e8151e7f9462
Sebastian Kollar <spkoll220803@gmail.com>
2024-09-29 15:59:40 +1000

    added libraries

--- src/main.h
+++ src/main.h
@@ -1,1 +1,27 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// misc posix stuff
+#include <unistd.h>
+
+// error handling
+#include <errno.h>
+
+// types library
+#include <sys/types.h>
+
+// sockets library
+#include <sys/socket.h>
+
+// net library??
+#include <netdb.h>
+
+#include <arpa/inet.h>
+
+#include <netinet/in.h>
+
+#include <sys/wait.h>
+
+#include <signal.h>
+
